86e8e2e5c034b3b37e185ab7728319e5debd185b
jingquan-wang <jq.wang1214@gmail.com>
2021-05-18 20:45:40 -0400

    clean repo

--- app/predict.py
+++
@@ -1,7 +0,0 @@
-"""
-- Load and read user-provided data
-- Featurization + feature selection
-- Prediction
-"""
-
-print("Hello World! I'm running!")
--- app/test.py
+++
@@ -1,3 +0,0 @@
-from mrp7pred.mrp7pred import MRP7Pred
-
-print("Hello World!")
